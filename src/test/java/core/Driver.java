16fc1eb6cd1a2988379887419650d1bb62bf4ec7
Ramesh Baskarasubramanian <rameshb@thoughtworks.com>
2017-10-25 11:16:15 +0530

    Minor changes to Driver class

--- src/test/java/core/Driver.java
+++ src/test/java/core/Driver.java
@@ -4,25 +4,42 @@ import com.thoughtworks.gauge.AfterSuite;
 import com.thoughtworks.gauge.BeforeSuite;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Driver {
+
     private static WebDriver webDriver = null;
+
+    private static void startDriver() {
+        String browser = System.getenv("BROWSER");
+
+        if (browser.toLowerCase().equals("firefox")) {
+            webDriver = new FirefoxDriver();
+        }
+
+        if (browser.toLowerCase().equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER_PATH"));
+            webDriver = new ChromeDriver();
+        }
+
+        webDriver.manage().window().maximize();
+    }
+
+    private static void stopDriver() {
+        if (webDriver != null) {
+            webDriver.quit();
+            webDriver = null;
+        }
+    }
 
     @BeforeSuite
     public void beforeSuite() {
-        if (webDriver == null) {
-            System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER_PATH"));
-            webDriver = new ChromeDriver();
-            webDriver.manage().window().maximize();
-        }
+        startDriver();
     }
 
     @AfterSuite
     public void afterSuite() {
-        if (webDriver != null) {
-            webDriver.close();
-            webDriver = null;
-        }
+        stopDriver();
     }
 
     public static WebDriver getDriver() {
